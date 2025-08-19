f4e67e534fc637702764074c0d4d018d29010796
Miguel <migueljm1008@gmail.com>
2025-08-19 21:32:43 +0200

    finished the tutorial with some extras

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const cowsay = require('cowsay');
+const bodyParser = require('body-parser');
+const request = require('request');
+const dotenv = require('dotenv');
+dotenv.config();
+const apiKey = process.env.API_KEY;
+const port = process.env.PORT || 3000;
+
+
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  res.render('index', {weather: null, error: null});
+})
+
+app.post('/', function (req, res) {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+})
+
+app.listen(port, () => {
+  console.log(
+    cowsay.say({
+      text: `Example app listening on port http://localhost:${port}`,
+      f: "tux", // Use the tux ASCII art // tux
+    })
+  );
+});
